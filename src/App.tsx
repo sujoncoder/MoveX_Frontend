e4148baa05a764a824ecae23f222ff8e6307e23d
sujoncoder <sujonsheikh.in@gmail.com>
2025-08-21 23:44:24 +0600

    project initialize

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Logo from "./shared/Logo"
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center text-2xl font-bold my-10 text-slate-500">Hello </h1>
+
+      <Logo />
+    </div>
+  )
+}
+
+export default App
---
+++ src/shared/Logo.tsx
@@ -0,0 +1,9 @@
+import logo from "../assets/images/logo.png";
+
+const Logo = () => {
+    return (
+        <img src={logo} width={150} height={150} alt="logo" />
+    )
+}
+
+export default Logo
